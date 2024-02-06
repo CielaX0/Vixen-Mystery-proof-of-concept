bb02fe8951774e52c370c96199174cbc7b06d52a
Ciela Lithelle <sonicsaul@gmail.com>
2024-02-06 05:45:01 -0500

    Add files via upload

---
+++ lines.py
@@ -0,0 +1,9 @@
+linesdict = {
+    1: "Hello?\n",
+    2: "Can you read this?",
+    3: "If you\'re reading this, it\'s likely that someone in your club has died.\n\n",
+    4: 'We need to get to the bottom of this before someone else falls victim to whatever is going on there.',
+    5: 'I know you have questions, but we don\'t have time for pleasantries at the moment. ',
+    6: 'For now, names will have to suffice.'
+        
+}
